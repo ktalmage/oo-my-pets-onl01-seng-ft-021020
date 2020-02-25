5c39b52facb72e9df90de2a13d4324a2f311e23c
Kyle Talmage <ktalmage85@gmail.com>
2020-02-25 21:39:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -75,6 +75,6 @@ class Owner
     
     def list_pets 
       
-      return "I have #{self.dogs.count} dogs and #{self.cats.count} cats."
+      return "I have #{self.dogs.count} dog(s) and #{self.cats.count} cat(s)."
     end
 end
